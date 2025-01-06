f61fbc4bfe73e01b3bb954899474454676619b49
laiscasagrande <laisluanaka@outlook.com>
2025-01-06 16:56:43 -0300

    :recycle: refactor: Convertendo para contexto

--- src/pages/Home/components/CountDown/index.tsx
+++ src/pages/Home/components/CountDown/index.tsx
@@ -1,14 +1,11 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { CountdownContainer, Separator } from "./styles";
 import { differenceInSeconds } from "date-fns";
+import { CyclesContext } from "../..";
 
-interface CountDownProps {
-  activeCycle: any;
-  setCycles: any;
-  activeCycleId: any
-}
-
-export function CountDown({activeCycle, setCycles, activeCycleId}: CountDownProps){
+export function CountDown(){
+  //obs: ao invés de passar a função setCycles para o contexto, criamos a função markCurrentCycleAsFinished. Por que fizemos isso? Porque a tipagem de setCycles ficara muito feia e fica mais performático fazer a função inteira lá na Home e só passar por contexto para usar neste componente 
+  const {activeCycle, activeCycleId, markCurrentCycleAsFinished} = useContext(CyclesContext)
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0) //ela vai guardar a quantidade de segundos que já se passaram desde que o ciclo se iniciou. Assim, conseguimos ir reduzindo desse total de segundos (totalSeconds) menos os segundos que já se passaram
    const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0//variável que vai converter o número de minutos que eu tenho no meu ciclo inserido pelo usuário em sgundos, porque é mais fácil eu trabalhar em segundos do que em minutos, porque o timer vai reduzir de segundo em segundo
     //Se houver um ciclo ativo, eu vou pegar a quantidade de minutos desse ciclo e converter para segundos
@@ -20,14 +17,15 @@ export function CountDown({activeCycle, setCycles, activeCycleId}: CountDownProp
           const secondsDifference = differenceInSeconds(new Date(), activeCycle.startDate) //a data atual e a data quando o ciclo começou
           //obs: quando atualizamos um estado e esse estado depende do seu valor anterior, devemos escrever isso em  um formato de função 
           if (secondsDifference >= totalSeconds) { //se o total de segundos que eu percorri já foi igual ou maior que o número de tempo que o meu ciclo tem eu marco como completo
-            setCycles((state) => state.map(cycle => { //percorrer o ciclo
-              //se o ciclo que estou percorrendo for o ciclo ativo, eu vou retornar todos os dados do ciclo, porém vou adicionar uma nova informação, que e a interruptedDate como a nova data, senão, retorno o ciclo sem alterações
-              if (cycle.id === activeCycleId) {
-                return { ...cycle, finishedDate: new Date() }
-              } else {
-                return cycle
-              }
-            }))
+            // setCycles((state) => state.map(cycle => { //percorrer o ciclo
+            //   //se o ciclo que estou percorrendo for o ciclo ativo, eu vou retornar todos os dados do ciclo, porém vou adicionar uma nova informação, que e a interruptedDate como a nova data, senão, retorno o ciclo sem alterações
+            //   if (cycle.id === activeCycleId) {
+            //     return { ...cycle, finishedDate: new Date() }
+            //   } else {
+            //     return cycle
+            //   }
+            // }))
+            markCurrentCycleAsFinished() //só chamos ela aqui. O código comentado acima é a função markCurrentCycleAsFinished que fiz no componente Home
             setAmountSecondsPassed(totalSeconds)
             clearInterval(interval)
           } else {
@@ -41,7 +39,22 @@ export function CountDown({activeCycle, setCycles, activeCycleId}: CountDownProp
         clearInterval(interval)
       }
   
-    }, [activeCycle, totalSeconds, activeCycleId])  //cada vez que a variável activeCycle mudar, esse código vai executar de novo
+    }, [activeCycle, totalSeconds, activeCycleId, markCurrentCycleAsFinished])  //cada vez que a variável activeCycle mudar, esse código vai executar de novo
+
+    const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0//A conta, o tanto que já passou. Se tiver um ciclo ativo, vai pegar o total de segundos menos o total de segundo que já passou
+
+    const minutesAmount = Math.floor(currentSeconds / 60)//Agora eu vou calcular quantos minutos eu tenho dentro desse total de segundos. O floor arredonda para baixo. Eu estou fazendo isso para mostrar isso em tela
+    const secondsAmount = currentSeconds % 60//Calcular quantos segundos eu tenho do resto da divisão de cima. Se de cima estou pegando o valor inteiro, aqui estou pegando o resto. 
+  
+    const minutes = String(minutesAmount).padStart(2, '0') //Convterter o número de minutos para uma string para usar o padStart. Este é um método que preenche uma string até um tamanho específico caso ela não tenha aquele tamanho ainda. A variável de minutos eu sempre quero que ela tenha dois caracteres, se ela não tiver dois caraceres, eu voi incluir 0 no começo, na start da string até completar como outro caracter
+    const seconds = String(secondsAmount).padStart(2, '0')
+  
+    useEffect(() => {
+      if (activeCycle) {
+        document.title = `${minutes}:${seconds}`
+      }
+    },
+      [minutes, seconds, activeCycle])//quando meus minutos ou segundos mudarem, eu quero atualizar o título da minha janela
 
   return(
         <CountdownContainer>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import { HandPalm, Play } from "phosphor-react";
-import {HomeContainer, StartCountdownButton, StopCountdownButton} from "./styles";
+import { HomeContainer, StartCountdownButton, StopCountdownButton } from "./styles";
 //import { useState } from "react";
-import { useEffect, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { differenceInSeconds } from 'date-fns' //calcula a diferença de duas datas em segundos
 import { NewCycleForm } from "./components/NewCycleForm";
 import { CountDown } from "./components/CountDown";
@@ -15,6 +15,14 @@ interface Cycle { //interface para definir qual vai ser o formato de cada ciclo 
   interruptedDate?: Date //data que o ciclo foi interrompido. Opcional porque la não necessariamente precisa interromper o clico
   finishedDate?: Date //guardar o valor do ciclo somente se ele foi encerrado
 }
+
+interface CyclesContextType { //aqui vamos falar quais são as informações que vamos armazenar dentro do contexto
+  activeCycle: Cycle | undefined //undefined porque enquanto o usuário não iniciar um novo ciclo, essa variável fica como undefined
+  activeCycleId: string | null //isso são estados que crie dentro deste componente e como quero usar em outros componentes, estou colocando aqui
+  markCurrentCycleAsFinished: () => void //função sem parâmetros e sem retorno nenhum
+}
+
+export const CyclesContext = createContext({} as CyclesContextType) //criação do contexto
 
 export function Home() { //se você passar o mouse em cima do useForm, você verá que o primeiro parâmetro que eu posso passar no generic é umm objeto, e o segundo é um any 
   //-> informação do ciclo:
@@ -30,26 +38,35 @@ export function Home() { //se você passar o mouse em cima do useForm, você ver
   //O método find percorre todo o array e traz o primeiro elemento que satisfaz a consdição
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)//mostrar na tela qual é o ciclo ativo. Com base no id do ciclo ativo, ele vai percorrer todos os ciclos e verificar quel ciclo tem o mesmo id do ciclo ativo
 
-  console.log("Ciclo ativo", activeCycle)
+  function markCurrentCycleAsFinished() { //marcar o ciclo atual como finalizado
+    setCycles((state) => state.map(cycle => { //percorrer o ciclo
+      //se o ciclo que estou percorrendo for o ciclo ativo, eu vou retornar todos os dados do ciclo, porém vou adicionar uma nova informação, que e a interruptedDate como a nova data, senão, retorno o ciclo sem alterações
+      if (cycle.id === activeCycleId) {
+        return { ...cycle, finishedDate: new Date() }
+      } else {
+        return cycle
+      }
+    }))
+  } 
 
   //Como estou usando o register, os valores task e minutesAmount são registrados, armazenados. Assim, quando o formulário for submetido, o react-hook-form coleta os dados registrados e passa esses valores para a função handleCreateNewCycle, que é a função chamada na hora da submissão, e, para acessar esses valores, você pode usar um parâmetro, que neste caso é o data
-  function handleCreateNewCycle(data: NewCycleFormData) { //esse data é um objeto que contém dois campos do nosso formulário(task e minutesAmount)
-    const id = String(new Date().getTime()) //converti a data para string porque definimos id como string
-    const newCycle: Cycle = {//criar um novo ciclo. Como eu coloquei :Cycle, ele já vai me trazer tudo que tem dentro de Cycle, ou seja, todas as informações que ue preciso para criar um novo ciclo
-      id,
-      task: data.task, //Eu estou usando o NewCycleFormData para pegar os valores task e minutesAmount que vem do formulário; e Cycle para guradar as informações de cada ciclo
-      minutesAmount: data.minutesAmount,
-      startDate: new Date //data atual, ou seja, a data que o ciclo iniciou
-    }
-    //console.log("aaaaa", newCycle)
-    setCycles((state) => [...state, newCycle])//adicionar meu novo ciclo a listagem de ciclos. Assim, para acresecentar um novo ciclo, eu preciso pegar todos os ciclos que ue já tenho e adiocinar o novo ciclo
-    setActiveCycleId(id) //passo para a função responsável por controlar se um ciclo está ativo ou não pelo id do ciclo, o id do ciclo. Essa função vai guardar na varável activeCycleId o id do ciclo.
-    setAmountSecondsPassed(0)
-    //Quando a função for chmamada, ele vai criar um id para o ciclo ( String(new Date().getTime())) e, ao mesmo tempo que ele criar, ele vai guardar esse id na função setActiveCycleId. Assim, o id criado quando o formulário for chamado vai ser o mesmo, pois está tudo na mesma função. Ele cria ali em cima e passa aqui(setActiveCycleId), por isso, o método sempre vai retornar true
-    //Além disso, sempre que uma alteração de estado depender do seu valor anterior, a gente usa o formato de arrow function e passa um argumento
-    //console.log(data) //como guardei em um register, ele guardou tudo que eu digitei
-    reset(); //ele limpa os campos, depois de o formulário ser submitado, para o valor inicial. Ele volta para o valor que eu defini dentro do defaultValues. 
-  }
+  // function handleCreateNewCycle(data: NewCycleFormData) { //esse data é um objeto que contém dois campos do nosso formulário(task e minutesAmount)
+  //   const id = String(new Date().getTime()) //converti a data para string porque definimos id como string
+  //   const newCycle: Cycle = {//criar um novo ciclo. Como eu coloquei :Cycle, ele já vai me trazer tudo que tem dentro de Cycle, ou seja, todas as informações que ue preciso para criar um novo ciclo
+  //     id,
+  //     task: data.task, //Eu estou usando o NewCycleFormData para pegar os valores task e minutesAmount que vem do formulário; e Cycle para guradar as informações de cada ciclo
+  //     minutesAmount: data.minutesAmount,
+  //     startDate: new Date //data atual, ou seja, a data que o ciclo iniciou
+  //   }
+  //   //console.log("aaaaa", newCycle)
+  //   setCycles((state) => [...state, newCycle])//adicionar meu novo ciclo a listagem de ciclos. Assim, para acresecentar um novo ciclo, eu preciso pegar todos os ciclos que ue já tenho e adiocinar o novo ciclo
+  //   setActiveCycleId(id) //passo para a função responsável por controlar se um ciclo está ativo ou não pelo id do ciclo, o id do ciclo. Essa função vai guardar na varável activeCycleId o id do ciclo.
+  //   setAmountSecondsPassed(0)
+  //   //Quando a função for chmamada, ele vai criar um id para o ciclo ( String(new Date().getTime())) e, ao mesmo tempo que ele criar, ele vai guardar esse id na função setActiveCycleId. Assim, o id criado quando o formulário for chamado vai ser o mesmo, pois está tudo na mesma função. Ele cria ali em cima e passa aqui(setActiveCycleId), por isso, o método sempre vai retornar true
+  //   //Além disso, sempre que uma alteração de estado depender do seu valor anterior, a gente usa o formato de arrow function e passa um argumento
+  //   //console.log(data) //como guardei em um register, ele guardou tudo que eu digitei
+  //   reset(); //ele limpa os campos, depois de o formulário ser submitado, para o valor inicial. Ele volta para o valor que eu defini dentro do defaultValues. 
+  // }
 
   function handleInterruptCycle() {
 
@@ -66,36 +83,23 @@ export function Home() { //se você passar o mouse em cima do useForm, você ver
   //Lembre-se que no react temos que seguir o conceito de imutabilidade, ou seja, temos que perorrer todo o array para aí então mudarmos a informação que desejamos. Aqui utilizamos o map. Por que?
   //Estou chamando a função setCycles, alterando o valor da variável que armazena os ciclos da aplicação, se estou alterando o valor, eu preciso dizer qual é o novo valor. Assim o map vai retornara cada ums dos ciclos e ele vai retornar de dentro do map cada um dos ciclos alterdos ou não 
 
-  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0//A conta, o tanto que já passou. Se tiver um ciclo ativo, vai pegar o total de segundos menos o total de segundo que já passou
-
-  const minutesAmount = Math.floor(currentSeconds / 60)//Agora eu vou calcular quantos minutos eu tenho dentro desse total de segundos. O floor arredonda para baixo. Eu estou fazendo isso para mostrar isso em tela
-  const secondsAmount = currentSeconds % 60//Calcular quantos segundos eu tenho do resto da divisão de cima. Se de cima estou pegando o valor inteiro, aqui estou pegando o resto. 
-
-  const minutes = String(minutesAmount).padStart(2, '0') //Convterter o número de minutos para uma string para usar o padStart. Este é um método que preenche uma string até um tamanho específico caso ela não tenha aquele tamanho ainda. A variável de minutos eu sempre quero que ela tenha dois caracteres, se ela não tiver dois caraceres, eu voi incluir 0 no começo, na start da string até completar como outro caracter
-  const seconds = String(secondsAmount).padStart(2, '0')
-
-  useEffect(() => {
-    if (activeCycle) {
-      document.title = `${minutes}:${seconds}`
-    }
-  },
-    [minutes, seconds, activeCycle])//quando meus minutos ou segundos mudarem, eu quero atualizar o título da minha janela 
-
-  const task = watch('task')//quero obervar o campo task, que foi o nome que eu dei no register. Agora consigo saber o valor do meu campo task em tempo real. Vou observar o campo task. Se ele for diferente de vazio, eu quero habilitar o botão  
-  const isSubmitDisabled = !task //meu botão vai estar desabilitado quando eu não tiver nada dentro da task. Para questões de legibilidade
+  //const task = watch('task')//quero obervar o campo task, que foi o nome que eu dei no register. Agora consigo saber o valor do meu campo task em tempo real. Vou observar o campo task. Se ele for diferente de vazio, eu quero habilitar o botão  
+ // const isSubmitDisabled = !task //meu botão vai estar desabilitado quando eu não tiver nada dentro da task. Para questões de legibilidade
 
   return (
     <HomeContainer>
-      <form onSubmit={handleSubmit(handleCreateNewCycle)} action=""> {/*passamos a função handleCreateNewCycle como argumento para a função handleSubmit */}
-       <NewCycleForm/>
-       <CountDown activeCycle={activeCycle} setCycles={setCycles} activeCycleId={activeCycleId}/>
+      <form /*onSubmit={handleSubmit(handleCreateNewCycle)}*/ action=""> {/*passamos a função handleCreateNewCycle como argumento para a função handleSubmit */}
+        <CyclesContext.Provider value={{activeCycle, activeCycleId, markCurrentCycleAsFinished}}>
+          {/*<NewCycleForm/>*/}
+          <CountDown/>
+        </CyclesContext.Provider>
         {activeCycle ? (
           <StopCountdownButton type="button" onClick={handleInterruptCycle}>
             <HandPalm size={24} />
             Interromper
           </StopCountdownButton>
         ) : (
-          <StartCountdownButton disabled={isSubmitDisabled} type="submit">
+          <StartCountdownButton /*disabled={isSubmitDisabled}*/ type="submit">
             <Play size={24} />
             Começar
           </StartCountdownButton>
@@ -104,4 +108,7 @@ export function Home() { //se você passar o mouse em cima do useForm, você ver
     </HomeContainer>
   )
 }
+//A função markCurrentCycleAsFinished foi definida no componente Home porque ela usa a função setCycles que só existe dentro do componente Home. 
+// Aí eu enviei essa função dentro do contexto e assim todos os componentes que estão dentro deste contexto tem acesso a ela(todos que estão dentro do provider). 
+// E quando o componente CountDown chama essa função, ele ta chamando na verdade a função que está aqui na Home, que vai alterar nosso estado de ciclos
 
