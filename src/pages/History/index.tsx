13e07cfbdc612cc98fd573c4dc33bff3cb793f58
laiscasagrande <laisluanaka@outlook.com>
2025-01-07 20:58:51 -0300

    :sparkles: feat: Listagem de histórico

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -4,13 +4,13 @@ import { CyclesContext } from "../../contexts/CyclesContext";
 
 
 export function History() {
-  const {cycles} = useContext(CyclesContext)
+  const { cycles } = useContext(CyclesContext)
   return (
     <HistoryContainer>
       <h1>Meu histórico</h1>
-<pre>
-  {JSON.stringify(cycles, null, 2)} {/*Mostrar em formato JSON e formater certinho*/}
-</pre>
+      {/* <pre>
+  {JSON.stringify(cycles, null, 2)} //Mostrar em formato JSON e formater certinho
+</pre> */}
       <HistoryList> {/*Criamos uma div para, quando estiver em dispositivos mobile, o usuário conseguir arrastar a tabela para o lado através de um scroll. E só podemos fazer isso por meio de uma div*/}
         <table>
           <thead>
@@ -22,42 +22,26 @@ export function History() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Tarefa</td>
-              <td>28 minutos</td>
-              <td>Há 2 meses</td>
-              <td><Status statusColor="green">Concluído</Status></td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>28 minutos</td>
-              <td>Há 2 meses</td>
-              <td><Status statusColor="green">Concluído</Status></td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>28 minutos</td>
-              <td>Há 2 meses</td>
-              <td><Status statusColor="green">Concluído</Status></td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>28 minutos</td>
-              <td>Há 2 meses</td>
-              <td><Status statusColor="green">Concluído</Status></td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>28 minutos</td>
-              <td>Há 2 meses</td>
-              <td><Status statusColor="yellow">Em andamento</Status></td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>28 minutos</td>
-              <td>Há 2 meses</td>
-              <td><Status statusColor="red">Interrompido</Status></td>
-            </tr>
+            {cycles.map(cycle => {
+              return (
+                <tr key={cycle.id}>
+                  <td>{cycle.task}</td>
+                  <td>{cycle.minutesAmount}</td>
+                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>
+                    {cycle.finishedDate && (
+                      <Status statusColor="green">Concluído</Status>
+                    )}
+                    {cycle.interruptedDate && (
+                      <Status statusColor="red">Interrompido</Status>
+                    )}
+                    {!cycle.finishedDate && !cycle.interruptedDate && ( //se eu não tiver uma finishedDate e nem uma interruptedDate 
+                      <Status statusColor="yellow">Em andamento</Status>
+                    )}
+                  </td>
+                </tr>
+              )
+            })}
           </tbody>
         </table>
       </HistoryList>
